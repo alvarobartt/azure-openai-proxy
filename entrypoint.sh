c989b720f383477c30597e36301f0e935218e01b
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-05-02 11:49:12 +0200

    Update `entrypoint.sh` to handle graceful shutdown

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+set -m
+
+trap 'kill -TERM $TGI_PID $PROXY_PID 2>/dev/null' TERM INT
+
 ldconfig 2>/dev/null || echo 'unable to refresh ld cache, not a big deal in most cases'
 
 source /usr/src/.venv/bin/activate
 
-trap 'kill -TERM $TGI_PID' TERM
 text-generation-launcher --port 8080 "$@" &
 TGI_PID=$!
 
 oaiaz &
 PROXY_PID=$!
 
-wait $PROXY_PID
-kill -TERM $TGI_PID
-wait $TGI_PID
+wait -n $TGI_PID $PROXY_PID
+
+kill -TERM $TGI_PID $PROXY_PID 2>/dev/null
+wait $TGI_PID $PROXY_PID 2>/dev/null
