0c0cc01d2416051bf37cfe7327f00e4d71db426d
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-05-02 11:35:28 +0200

    Add `tracing` and `tracing-subscriber`

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,21 @@ use axum::{
     Router,
 };
 use hyper_util::{client::legacy::connect::HttpConnector, rt::TokioExecutor};
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 type HttpClient = hyper_util::client::legacy::Client<HttpConnector, Body>;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            fmt::layer()
+                .with_target(true)
+                .with_level(true)
+                .with_filter(EnvFilter::from_default_env()),
+        )
+        .init();
+
     let client: HttpClient =
         hyper_util::client::legacy::Client::<(), ()>::builder(TokioExecutor::new())
             .build(HttpConnector::new());
@@ -21,7 +31,11 @@ async fn main() {
         .with_state(client);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
-    println!("listening on {}", listener.local_addr().unwrap());
+    tracing::info!(
+        target: "oaiaz-proxy",
+        "Listening on {}",
+        listener.local_addr().unwrap()
+    );
     axum::serve(listener, app).await.unwrap();
 }
 
@@ -40,12 +54,25 @@ async fn handler(
         let query = req.uri().query().unwrap_or_default();
         let new_uri = format!("http://0.0.0.0:8080/v1/chat/completions?{}", query);
 
-        *req.uri_mut() = Uri::try_from(new_uri).map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+        *req.uri_mut() = Uri::try_from(new_uri).map_err(|_| {
+            tracing::info!(target: "oaiaz-proxy", "URI rewrite failed for: {}", req.uri());
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
     }
+
+    tracing::info!(
+        target: "oaiaz-proxy",
+        "Proxying {} request to {}",
+        req.method(),
+        req.uri()
+    );
 
     client
         .request(req)
         .await
-        .map_err(|_| StatusCode::BAD_GATEWAY)
+        .map_err(|e| {
+            tracing::info!(target: "oaiaz-proxy", "Proxy error: {}", e);
+            StatusCode::BAD_GATEWAY
+        })
         .map(|res| res.into_response())
 }
