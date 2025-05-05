04abb2a784ffc360e1b778e57cab10b3b9bf474f
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-05-05 12:39:05 +0200

    Update `tgi-entrypoint.sh`

--- tgi-entrypoint.sh
+++ tgi-entrypoint.sh
@@ -8,10 +8,17 @@ ldconfig 2>/dev/null || echo 'unable to refresh ld cache, not a big deal in most
 
 source /usr/src/.venv/bin/activate
 
-UPSTREAM_HOST="host.docker.internal"
-export UPSTREAM_HOST="$UPSTREAM_HOST"
+export UPSTREAM_HOST="host.docker.internal"
 
-UPSTREAM_PORT=8080
+UPSTREAM_PORT="${UPSTREAM_PORT:-8080}"
+if [[ -n "$UPSTREAM_PORT" ]]; then
+    if [[ "$UPSTREAM_PORT" -eq 80 ]]; then
+        echo "ERROR: UPSTREAM_PORT environment variable cannot be set to 80 when running text-generation-inference with Docker, as the port 80 is reserved for the openai-azure-proxy service."
+        exit 1
+    fi
+    UPSTREAM_PORT=$UPSTREAM_PORT
+fi
+
 export UPSTREAM_PORT="$UPSTREAM_PORT"
 unset PORT
 
