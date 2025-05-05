588ef428e68f455e4fe5f0015f637316196d45c7
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-05-05 18:25:14 +0200

    Update `tgi-entrypoint.sh`
    - Align double-quote usage over single-quotes
    - Add `HF_HUB_USER_AGENT_ORIGIN`

--- tgi-entrypoint.sh
+++ tgi-entrypoint.sh
@@ -2,15 +2,18 @@
 
 set -m
 
-trap 'kill -TERM $UPSTREAM_PID $PROXY_PID 2>/dev/null' TERM INT
+trap "kill -TERM $UPSTREAM_PID $PROXY_PID 2>/dev/null" TERM INT
 
-ldconfig 2>/dev/null || echo 'unable to refresh ld cache, not a big deal in most cases'
+ldconfig 2>/dev/null || echo "Unable to refresh ld cache"
 
 source /usr/src/.venv/bin/activate
 
 export UPSTREAM_HOST="http://localhost"
 
+# TODO: we should also skip the `-p/--port` argument handling and make sure that's not set to 80,
+# and that only one of `PORT`, `UPSTREAM_PORT` or `-p/--port` is set
 UPSTREAM_PORT="${UPSTREAM_PORT:-8080}"
+
 if [[ -n "$UPSTREAM_PORT" ]]; then
     if [[ "$UPSTREAM_PORT" -eq 80 ]]; then
         echo "ERROR: UPSTREAM_PORT environment variable cannot be set to 80 when running text-generation-inference with Docker, as the port 80 is reserved for the openai-azure-proxy service."
@@ -22,8 +25,8 @@ fi
 export UPSTREAM_PORT="$UPSTREAM_PORT"
 unset PORT
 
-# TODO: we should also skip the `-p/--port` argument handling and make sure that's not set to 80,
-# and that only one of `PORT`, `UPSTREAM_PORT` or `-p/--port` is set
+export HF_HUB_USER_AGENT_ORIGIN="azure:foundry:gpu-cuda:inference:tgi-native" 
+
 text-generation-launcher --port "${UPSTREAM_PORT}" "$@" &
 UPSTREAM_PID=$!
 
