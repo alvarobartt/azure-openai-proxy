2d351c515bdc22b1d602d965a5a203a5905f6b72
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-05-06 17:25:52 +0200

    Update `tgi-entrypoint.sh`
    - Add `PROXY_{HOST,PORT}` handling via environment variable
    - Add some comments and update based on latest updates

--- tgi-entrypoint.sh
+++ tgi-entrypoint.sh
@@ -8,12 +8,16 @@ ldconfig 2>/dev/null || echo "Unable to refresh ld cache"
 
 source /usr/src/.venv/bin/activate
 
-export UPSTREAM_HOST="http://localhost"
+PROXY_HOST="${HOST:-0.0.0.0}"
+PROXY_PORT="${PORT:-80}"
+# We need to unset the HOST and the PORT because otherwise it's picked up by TGI
+unset HOST PORT
+
+UPSTREAM_HOST="0.0.0.0"
+UPSTREAM_PORT="${UPSTREAM_PORT:-8080}"
 
 # TODO: we should also skip the `-p/--port` argument handling and make sure that's not set to 80,
 # and that only one of `PORT`, `UPSTREAM_PORT` or `-p/--port` is set
-UPSTREAM_PORT="${UPSTREAM_PORT:-8080}"
-
 if [[ -n "$UPSTREAM_PORT" ]]; then
     if [[ "$UPSTREAM_PORT" -eq 80 ]]; then
         echo "ERROR: UPSTREAM_PORT environment variable cannot be set to 80 when running text-generation-inference with Docker, as the port 80 is reserved for the openai-azure-proxy service."
@@ -22,15 +26,12 @@ if [[ -n "$UPSTREAM_PORT" ]]; then
     UPSTREAM_PORT=$UPSTREAM_PORT
 fi
 
-export UPSTREAM_PORT="$UPSTREAM_PORT"
-unset PORT
-
 export HF_HUB_USER_AGENT_ORIGIN="azure:foundry:gpu-cuda:inference:tgi-native" 
 
-text-generation-launcher --port "${UPSTREAM_PORT}" "$@" &
+text-generation-launcher --host "$UPSTREAM_HOST" --port "$UPSTREAM_PORT" "$@" &
 UPSTREAM_PID=$!
 
-openai-azure-proxy --host 0.0.0.0 --port 80 --upstream-host "${UPSTREAM_HOST}" --upstream-port "${UPSTREAM_PORT}" &
+openai-azure-proxy --host "$PROXY_HOST" --port "$PROXY_PORT" --upstream-host "$UPSTREAM_HOST" --upstream-port "$UPSTREAM_PORT" &
 PROXY_PID=$!
 
 wait -n $UPSTREAM_PID $PROXY_PID
