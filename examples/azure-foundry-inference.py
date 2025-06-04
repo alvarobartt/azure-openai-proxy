99d8008303b0db174c2e2af410b6f56146111016
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-06-04 14:39:38 +0000

    Add `examples/azure-foundry-inference.py`

--- examples/azure-foundry-inference.py
+++ examples/azure-foundry-inference.py
@@ -1,1 +1,16 @@
-# Reference: https://learn.microsoft.com/en-us/python/api/overview/azure/ai-inference-readme?view=azure-python-preview
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage, UserMessage
+
+if __name__ == "__main__":
+    client = ChatCompletionsClient(endpoint="http://localhost", credential="")  # type: ignore
+
+    response = client.complete(
+        messages=[
+            SystemMessage("You are a helpful assistant."),
+            UserMessage("How many feet are in a mile?"),
+        ],
+        max_tokens=128,
+    )
+
+    print(response.choices[0].message.content)
+    print(f"\nToken usage: {response.usage}")
