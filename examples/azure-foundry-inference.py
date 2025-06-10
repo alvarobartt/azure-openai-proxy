903cc3a23dc9cd6fa7993892327c419e06987d92
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-06-10 10:47:50 +0000

    Update `examples/azure-foundry-inference.py`

--- examples/azure-foundry-inference.py
+++ examples/azure-foundry-inference.py
@@ -1,3 +1,10 @@
+# /// script
+# dependencies = [
+#   "azure-ai-inference",
+#   "azure-identity",
+# ]
+# ///
+
 from azure.ai.inference import ChatCompletionsClient
 from azure.ai.inference.models import SystemMessage, UserMessage
 
@@ -5,11 +12,12 @@ if __name__ == "__main__":
     client = ChatCompletionsClient(endpoint="http://localhost", credential="")  # type: ignore
 
     response = client.complete(
+        model="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
         messages=[
             SystemMessage("You are a helpful assistant."),
             UserMessage("How many feet are in a mile?"),
         ],
-        max_tokens=128,
+        max_tokens=256,
     )
 
     print(response.choices[0].message.content)
